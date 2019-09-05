73a68c693b32f4fe022b52ca1a9dfb855be274f3
Vajahath Ahmed <vajahath@qburst.com>
2019-09-05 18:13:03 +0530

    initial attempt to script loader

---
+++ src/index.ts
@@ -0,0 +1,110 @@
+export interface IDynamicScript {
+  scr: string;
+  opt?: IScriptLoaderOpt;
+}
+export type IScriptLoaderOpt = Partial<I_SCRIPT_LOADER_CONFIG_RAW>;
+
+interface I_SCRIPT_LOADER_CONFIG_RAW {
+  async: boolean;
+  attrs: object;
+  type: 'text/javascript' | string;
+}
+
+const DEFAULTS: IScriptLoaderOpt = {
+  async: true,
+  attrs: {},
+  type: 'text/javascript',
+};
+
+export function scriptLoader(dynamicScripts: IDynamicScript[], doc = document) {
+  // get all loaded scripts
+  const allScriptsTags = doc.getElementsByTagName('script');
+  const allLoadedScripts: string[] = [];
+  {
+    let i = 0;
+    for (; i < allScriptsTags.length; i++) {
+      const src = allScriptsTags[i].getAttribute('src');
+      if (src) {
+        allLoadedScripts.push(src);
+      }
+    }
+  }
+
+  const loadPromises: Array<Promise<any>> = [];
+  // load said scripts
+  for (const src of dynamicScripts) {
+    if (allLoadedScripts.indexOf(src.scr) >= 0) {
+      continue;
+    }
+    loadPromises.push(loadScript(src, doc));
+  }
+
+  return Promise.all(loadPromises);
+}
+
+function loadScript(src: IDynamicScript, doc: Document) {
+  return new Promise(resolve => {
+    const node = doc.createElement('script');
+
+    // Set Attributes
+    setAttributes(node, src.opt);
+
+    const host =
+      doc.getElementsByTagName('head')[0] ||
+      doc.getElementsByTagName('body')[0];
+
+    if (!host) {
+      throw new Error('No <head> or <body> tags found.');
+    }
+
+    host.appendChild(node);
+
+    // after load events
+    // cross browser handling of onLoaded event
+    if ((node as any).readyState) {
+      // IE
+      (node as any).onreadystatechange = () => {
+        if (
+          (node as any).readyState === 'loaded' ||
+          (node as any).readyState === 'complete'
+        ) {
+          (node as any).onreadystatechange = null;
+          return resolver();
+        }
+      };
+    } else {
+      // Others
+      node.onload = function() {
+        return resolver();
+      };
+    }
+
+    // node.src = src;
+    node.setAttribute('src', src.scr);
+
+    function resolver() {
+      console.log(node);
+      return resolve();
+    }
+  });
+}
+
+function setAttributes(
+  node: HTMLScriptElement,
+  opt: IScriptLoaderOpt = DEFAULTS,
+) {
+  const mergedOpt = Object.assign({}, opt, DEFAULTS);
+
+  if (mergedOpt.async) {
+    node.setAttribute('async', '');
+  }
+  if (mergedOpt.type) {
+    node.setAttribute('type', mergedOpt.type);
+  }
+
+  Object.entries(mergedOpt.attrs || {}).forEach(entries => {
+    node.setAttribute(...entries);
+  });
+
+  return node;
+}
